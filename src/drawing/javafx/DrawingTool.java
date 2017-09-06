5831fe4dbc03130d65c16e3c83a48dab98178abb
Ruud Schouten <ruud.schouten@outlook.com>
2017-09-06 16:26:42 +0200

    Changed FXCanvas to GraphicsContext Implemented Draw

--- src/drawing/javafx/DrawingTool.java
+++ src/drawing/javafx/DrawingTool.java
@@ -2,18 +2,18 @@ package drawing.javafx;
 
 import drawing.domain.Drawing;
 import drawing.javafx.JavaFXPaintable;
-import javafx.embed.swt.FXCanvas;
+import javafx.scene.canvas.GraphicsContext;
 
 public class DrawingTool {
     private Drawing drawing;
     private JavaFXPaintable paintable;
 
-    public DrawingTool (FXCanvas canvas, Drawing drawing) {
+    public DrawingTool (GraphicsContext gc, Drawing drawing) {
         this.drawing = drawing;
-        paintable = new JavaFXPaintable(canvas);
+        paintable = new JavaFXPaintable(gc);
     }
 
     public void draw() {
-
+        drawing.paintUsing(paintable);
     }
 }
